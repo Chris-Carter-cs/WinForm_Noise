d27441a88a536a9f3efe3a5d1aa13f0d94df8fb4
chris-carter-cs <Chris.carter.cs@gmail.com>
2024-04-06 20:08:14 -0500

    Add project files.

---
+++ WinForm_Noise/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WinForm_Noise
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            MapDisplayBox = new PictureBox();
+            btn_generate = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)MapDisplayBox).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            groupBox1.AutoSize = true;
+            groupBox1.Controls.Add(MapDisplayBox);
+            groupBox1.Location = new Point(288, 12);
+            groupBox1.MinimumSize = new Size(500, 400);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(500, 400);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // MapDisplayBox
+            // 
+            MapDisplayBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            MapDisplayBox.Location = new Point(3, 3);
+            MapDisplayBox.Name = "MapDisplayBox";
+            MapDisplayBox.Size = new Size(475, 375);
+            MapDisplayBox.TabIndex = 0;
+            MapDisplayBox.TabStop = false;
+            // 
+            // btn_generate
+            // 
+            btn_generate.Location = new Point(157, 389);
+            btn_generate.Name = "btn_generate";
+            btn_generate.Size = new Size(104, 23);
+            btn_generate.TabIndex = 1;
+            btn_generate.Text = "Generate Map";
+            btn_generate.UseVisualStyleBackColor = true;
+            btn_generate.Click += btn_generate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_generate);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)MapDisplayBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private PictureBox MapDisplayBox;
+        private Button btn_generate;
+    }
+}
---
+++ WinForm_Noise/Generator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForm_Noise
+{
+    internal class Generator
+    {
+
+        public static Bitmap DummyMap()
+        {
+            Bitmap bmp = new Bitmap(500, 400);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for(int y = 0; y < bmp.Height; y++)
+                {
+
+                    bmp.SetPixel(x, y, Color.DarkKhaki);
+
+                }
+            }
+            return bmp;
+        }
+
+        public static Bitmap SimpleNoise()
+        {
+            Bitmap bmp = new Bitmap(500, 400);
+            Random random = new Random();
+
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    byte sample = (byte)random.Next(255);
+                    Color c = Color.FromArgb(sample, sample, sample);
+                    bmp.SetPixel(x, y, c);
+
+                }
+            }
+            return bmp;
+        }
+
+        public static Bitmap PerlinNoise()
+        {
+            Bitmap bmp = new Bitmap(500, 400);
+            Random random = new Random();
+            int imageHeight = 400;
+            int imageWidth = 500;
+            int gridHeight = 8;
+            int gridWidth = 10;
+
+            //Generate gradiant vectors.
+            Vector2[,] gradiants = new Vector2[gridWidth, gridHeight];
+            for (int gridX = 0; gridX < gridWidth; gridX++)
+            {
+                for(int gridY = 0; gridY < gridHeight; gridY++)
+                {
+                    float angle = (float)random.NextDouble() * 360;
+                    float x = (float)Math.Sin(angle);
+                    float y = (float)Math.Cos(angle);
+
+                    gradiants[gridX, gridY] = new Vector2(x, y);
+                }
+            }
+
+            //Calculate the offset vectors for each point
+            for(int x = 0; x < imageWidth; x++)
+            {
+                for(int y = 0; y < imageHeight; y++)
+                {
+                    //Find ratio of x/y pixel position to total image size
+                    float fx = x;
+                    fx = fx / imageWidth;
+                    float fy = y;
+                    fy = fy / imageHeight;
+
+                    if (false && y == 25 && x % 100 == 25)
+                    {
+                        Debug.Write(string.Format("Pixel ({0}, {1}) goes to ({2}, {3})", x, y, fx, fy));
+                    }
+
+                    //Use that to translate to be in terms of the grid
+                    fx *= gridWidth;
+                    fy *= gridHeight;
+
+                    //Round to the nearest integer to index grid array
+                    int gx = (int)Math.Round(fx);
+                    int gy = (int)Math.Round(fy);
+
+                    //Find offset vector in terms of the grid space.
+                    Vector2 offset = new Vector2(gx - fx, gy - fy);
+
+                    //Take the dot product of the offset vector and the gradiant vector.
+                    float dot;
+                    
+                    //Make sure that the gx and gy coordinates are valid. If not, use a dummy value for the dot (for now).
+                    if(gx >= 0 && gx < gridWidth && gy >= 0 && gy < gridHeight)
+                    {
+                         dot = Vector2.Dot(gradiants[gx, gy], offset);
+                    } else
+                    {
+                        dot = 0.5f;
+                    }
+
+                    //Interpolate dot product between 0 and 255.
+                    byte iDot = (byte)((dot * 255) % 255);
+
+                    //Set the pixel on the map.
+                    Color c = Color.FromArgb(iDot, iDot, iDot);
+                    bmp.SetPixel(x, y, c);
+                }
+            }
+           
+
+            
+            return bmp;
+        }
+
+    }
+}
